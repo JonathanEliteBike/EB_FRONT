2102e73ca21ec946f1bba8a9504240921ca1369b
sistemas3 <sistemas.3@elitebike-mx.com>
2026-07-22 13:35:17 -0600

    UPD: Se actualizaron los datos de la calculadora de retroactivos, así como el agregado las siguientes condiciones:
    Si la clasificación seleccionada es menor a la sugerida, solo se da un retroactivo del 1.5%.
    
    El polígono exclusivo, solo se asigna al nivel Partner Elite en adelante.

--- src/app/views/internal-views/calculadora-retroactivos/models/programa-especial-impulso.model.ts
+++ src/app/views/internal-views/calculadora-retroactivos/models/programa-especial-impulso.model.ts
@@ -4,7 +4,16 @@ export interface ProgramaEspecialImpulso{
 }
 
 export const LISTA_PROGRAMA_ESPECIAL_IMPULSO: ProgramaEspecialImpulso[] = [
-    { descripcion: "Programa de BICICLETAS Electricas" , valor: "Consiste en realizar una compra de al menos 1 y como máximo 6 bicicletas eRIDE o EBikes en el periodo de Julio de 2026 a Diciembre de 2026, como parte del programa de bicicletas Demo y para fomentar el uso de bicicletas eléctricas. Beneficios: Línea de crédito adicional** a la línea actual (limitada a un máximo del 50% del monto de la línea actual). Apica sobre bicicletas nuevas gama 2027, 40% descuento sobre precio al Público Plazo especial de pago de 10 mensualidades."},
-    { descripcion: "APPAREL SCOTT", valor: "Consiste en impulsar todas las líneas de apparel (ropa, zapatos, cascos, guantes, lentes, protecciones, bolsas y mochilas) desde inicio de temporada a través de su exhibición y disponibilidad  en los puntos de venta. Beneficios: Línea de crédito adicional** a la línea actual, para la compra de Apparel en todas sus líneas (ropa, zapatos, cascos, guantes, lentes, protecciones, bolsas y mochilas), por un importe igual al 50% de la compra mínima de Apparel estipulada en cada nivel de distribuidor. Plazo especial de pago de 60, 90, o 120 días."},
-    { descripcion: "PROGRAMA COMPRA DE EMPLEADOS" , valor: "Con la finalidad de apoyar a la fuerza de venta para adquirir equipo de la marca SCOTT, ofrecemos el programa de compra de bicicleta y equipo para empleados el cual consiste en: 43% descuento sobre precio al público, de hasta una bicicleta por temporada, un casco, dos pares de ruedas y un par de zapatos."},
+    { 
+        descripcion: "PROGRAMA DE BICICLETAS ELÉCTRICAS", 
+        valor: "CONSISTE EN REALIZAR LA COMPRA DE AL MENOS 1 Y COMO MÁXIMO 6 BICICLETAS E-RIDE O E-BIKES DURANTE EL PERIODO DE JULIO DE 2026 A DICIEMBRE DE 2026, COMO PARTE DEL PROGRAMA DE BICICLETAS DEMO Y PARA FOMENTAR EL USO DE BICICLETAS ELÉCTRICAS. BENEFICIOS: LÍNEA DE CRÉDITO ADICIONAL** A LA LÍNEA DE CRÉDITO ACTUAL (LIMITADA A UN MÁXIMO DEL 50 % DEL MONTO DE LA LÍNEA ACTUAL). APLICA PARA BICICLETAS NUEVAS GAMA 2027. 40 % DE DESCUENTO SOBRE EL PRECIO AL PÚBLICO. PLAZO ESPECIAL DE PAGO DE HASTA 10 MENSUALIDADES."
+    },
+    { 
+        descripcion: "APPAREL SCOTT", 
+        valor: "CONSISTE EN IMPULSAR TODAS LAS LÍNEAS DE APPAREL (ROPA, ZAPATOS, CASCOS, GUANTES, LENTES, PROTECCIONES, BOLSAS Y MOCHILAS) DESDE EL INICIO DE LA TEMPORADA A TRAVÉS DE SU EXHIBICIÓN Y DISPONIBILIDAD EN LOS PUNTOS DE VENTA. BENEFICIOS: LÍNEA DE CRÉDITO ADICIONAL** A LA LÍNEA DE CRÉDITO ACTUAL PARA LA COMPRA DE APPAREL EN TODAS SUS LÍNEAS (ROPA, ZAPATOS, CASCOS, GUANTES, LENTES, PROTECCIONES, BOLSAS Y MOCHILAS), POR UN IMPORTE EQUIVALENTE AL 50 % DE LA COMPRA MÍNIMA DE APPAREL ESTIPULADA PARA CADA NIVEL DE DISTRIBUIDOR. PLAZO ESPECIAL DE PAGO DE 60, 90 O 120 DÍAS."
+    },
+    { 
+        descripcion: "PROGRAMA COMPRA DE EMPLEADOS", 
+        valor: "CON LA FINALIDAD DE APOYAR A LA FUERZA DE VENTAS PARA ADQUIRIR EQUIPO DE LA MARCA SCOTT, OFRECEMOS EL PROGRAMA DE COMPRA DE BICICLETAS Y EQUIPO PARA EMPLEADOS, EL CUAL CONSISTE EN: 43 % DE DESCUENTO SOBRE EL PRECIO AL PÚBLICO EN LA COMPRA DE HASTA UNA BICICLETA POR TEMPORADA, UN CASCO, DOS PARES DE RUEDAS Y UN PAR DE ZAPATOS."
+    },
 ]
